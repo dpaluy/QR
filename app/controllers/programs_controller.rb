0c316cf02a697187d9a85ce9f1046750d79153a4
dpaluy <dpaluy@c3da5284-7a2d-4169-a6cf-ff33e6757432>
2009-07-01 21:17:04 +0000

    xml ready

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,2 +1,11 @@
 class ProgramsController < ResourceController::Base
+
+	def show
+		@program = Program.find(params[:id])
+
+		respond_to do |format|
+                format.html  # do nothing, allow Rails to render index.rhtml
+                format.xml   { render :xml => @program.to_xml }
+        end
+	end
 end
