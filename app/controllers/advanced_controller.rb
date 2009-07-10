bca000a045784ef7ab0dbd1bcfc42c9b67ef2dde
dpaluy <dpaluy@c3da5284-7a2d-4169-a6cf-ff33e6757432>
2009-07-10 17:25:25 +0000

    Twitter update

--- app/controllers/advanced_controller.rb
+++ app/controllers/advanced_controller.rb
@@ -9,20 +9,4 @@ class AdvancedController < ApplicationController
 	end
   end
 
-  def tweet
-    if (!params[:program_id].nil? && program = Program.find(params[:program_id]))
-		begin
-			httpauth = Twitter::HTTPAuth.new('NDSTI', "samara#78")
-			base = Twitter::Base.new(httpauth)
-			base.update("I am watching very interesting program '#{program.name}'")
-			falsh[:notice] = "Twitter updated successfuly!"
-		rescue
-			flash[:error] = "Error updating Twitter!"
-		end
-	else
-		flash[:error] = "Please specify program id to Tweet"
-	end
-    redirect_to :back
-  end
-
 end
---
+++ app/controllers/tweet_controller.rb
@@ -0,0 +1,24 @@
+class TweetController < ApplicationController
+  
+  before :login_required
+
+  def index
+  end
+
+  def update
+	if params[:msg].nil? 
+		program = Program.find(params[:program_id])
+		message = "I am watching cool program '#{program.name}'"	
+	else
+		message = params[:msg]
+	end
+    @tweet = current_user.twitter.post('/statuses/update.json', :status => message) 
+	if 	@tweet.nil?
+		flash[:error] = "Error updating Twitter!"
+	else
+		flash[:notice] = "Tweeting successfully:\n #{@tweet.text}"
+	end
+	redirect_back_or_default "/advanced"
+  end
+
+end
